d2c98189a5c9855ef5a882a66d8fa9f1aee6cf2b
Ping <44315881+Ping6666@users.noreply.github.com>
2022-01-12 15:58:02 +0800

    envSetup

--- envSetup/mysetup.sh
+++ envSetup/mysetup.sh
@@ -15,10 +15,17 @@ rawDataDir='rawData'
 postDataDir='postData'
 
 # setup sys env
-sudo apt -qq install python3-pip=20.0.2-5ubuntu1.6 -y
+sudo apt-get -qq install python3-pip=20.0.2-5ubuntu1.6 -y
+
+# setup uwsgi env
+sudo apt-get -qq install build-essential=12.8ubuntu1.1 -y
+sudo apt-get -qq install python3-dev=3.8.2-0ubuntu2 -y
+pip install -q -v uwsgi==2.0.20
+mkdir ~/www/
+touch ~/www/my_stock_site.sock
 
 # setup virtualenv
-pip install -v -qq virtualenv==20.13.0
+pip install -q -v virtualenv==20.13.0
 cd $projectBaseDir
 echo "current runing dir: ${PWD}"
 ~/.local/bin/virtualenv -q $projectDirName
@@ -27,17 +34,12 @@ echo "current runing dir: ${PWD}"
 cp -r ${PWD}/../../Stock-Project-main/ $projectBaseDir/$projectDirName/
 mv $projectBaseDir/$projectDirName/Stock-Project-main/ $projectBaseDir/$projectDirName/code/
 
-# setup uwsgi env
-pip install -v -qq uwsgi==2.0.20
-mkdir ~/www/
-touch ~/www/my_stock_site.sock
-
 # setup nginx env
-sudo apt -qq install nginx=1.18.0-0ubuntu1.2 -y
+sudo apt-get -qq install nginx=1.18.0-0ubuntu1.2 -y
 sudo cp ./default /etc/nginx/sites-enabled/
 
 # setup supervisor env
-sudo apt -qq install supervisor=4.1.0-1ubuntu1 -y
+sudo apt-get -qq install supervisor=4.1.0-1ubuntu1 -y
 sudo cp ./mysite.conf /etc/supervisor/conf.d/
 sudo supervisorctl reread
 sudo supervisorctl update
