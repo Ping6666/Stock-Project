dd2cb782e2f98b6878ffb432555d7e2d530a424f
Ping <44315881+Ping6666@users.noreply.github.com>
2022-04-19 18:52:18 +0800

    Add files via upload

--- envSetup/firstsetup.sh
+++ envSetup/firstsetup.sh
@@ -1,60 +1,79 @@
 #!/bin/sh
 # first you need to setup files under dir ./envSetup/
-# 1. for uwsgi under file myuwsgi.ini: userhome, socket
-# 2. for nginx under file default: ip addr, server unix dir
-# 3. for supervisor under file mysite.conf: directory, command
-# 4. if env is Raspberry_Pi deal all line with '# For Raspberry_Pi'
+# if env is Raspberry_Pi deal all line with '# For Raspberry_Pi'
 
 # second decide where to store the project
-projectBaseDir='/home/ping' # plz give a(n) (absolute) path
+projectBaseDir='/home/ping' # plz give a(n) (absolute) path (user home)
+serverIp='192.168.0.1'
+
 projectDirName='stockproject'
 pipArgv=' --no-cache-dir --extra-index-url https://www.piwheels.org/simple ' # For Raspberry_Pi
 
 # sh pre-set
 runningBaseDir=${PWD}
+projectDefualtName='Stock-Project-main'
 envSetupDir='envSetup'
 rawDataDir='rawData'
 postDataDir='postData'
 
-# setup sys env
-sudo apt-get update
-sudo apt-get -qq install python3-pip=20.0.2-5ubuntu1.6 -y
+function replaceFileTextBysearch {
+    # $1: search, $2: replace, $3: filename
+    if [[ $1 != "" && $2 != "" ]]; then
+        echo "target file: $3, with $1 change to $2."
+        sed -i "s@$1@$2@g" $3
+    fi
+}
 
-# setup uwsgi env
-sudo apt-get -qq install build-essential=12.8ubuntu1.1 -y
-sudo apt-get -qq install python3-dev=3.8.2-0ubuntu2 -y
-pip install -q ${pipArgv} -v uwsgi==2.0.20
-mkdir ${projectBaseDir}/www/
-touch ${projectBaseDir}/www/my_stock_site.sock
-
-# setup virtualenv
-pip install -q ${pipArgv} -v virtualenv==20.13.0
 cd $projectBaseDir
 echo "current runing dir: ${PWD}"
-${projectBaseDir}/.local/bin/virtualenv -q $projectDirName
-cd $runningBaseDir/$(dirname "$0")
-echo "current runing dir: ${PWD}"
-cp -r ${PWD}/../../Stock-Project-main/ $projectBaseDir/$projectDirName/
-mv $projectBaseDir/$projectDirName/Stock-Project-main/ $projectBaseDir/$projectDirName/code/
 
-# setup nginx env
-sudo apt-get -qq install nginx=1.18.0-0ubuntu1.2 -y
+# replace for custom file text
+replaceFileTextBysearch "PWD_DIR" "$projectBaseDir" "./$projectDefualtName/$envSetupDir/mysite.conf"
+replaceFileTextBysearch "PWD_DIR" "$projectBaseDir" "./$projectDefualtName/$envSetupDir/myuwsgi.ini"
+replaceFileTextBysearch "PWD_DIR" "$projectBaseDir" "./$projectDefualtName/$envSetupDir/default"
+replaceFileTextBysearch "Server_IP" "$serverIp" "./$projectDefualtName/$envSetupDir/default"
+
+sudo apt-get update
+sudo apt-get -qq install python3-pip build-essential python3-dev nginx supervisor -y
+sudo pip install -q -v uwsgi==2.0.20 virtualenv==20.13.0
+
+# For Raspberry_Pi
+# sudo pip install -q $pipArgv -v uwsgi==2.0.20 virtualenv==20.13.0
+
+mkdir $projectBaseDir/www/
+touch $projectBaseDir/www/my_stock_site.sock
 sudo cp ./default /etc/nginx/sites-enabled/
-
-# setup supervisor env
-sudo apt-get -qq install supervisor=4.1.0-1ubuntu1 -y
 sudo cp ./mysite.conf /etc/supervisor/conf.d/
 sudo supervisorctl reread
 sudo supervisorctl update
 
-# setup python main env
-# sudo apt-get -qq install libxml2-dev libxslt-dev python-dev -y # For Raspberry_Pi
+virtualenv -q $projectDirName
+
+cd $runningBaseDir/$(dirname "$0")
+echo "current runing dir: ${PWD}"
+
+cp -r ${PWD}/../../$projectDefualtName/ $projectBaseDir/$projectDirName/
+mv $projectBaseDir/$projectDirName/$projectDefualtName/ $projectBaseDir/$projectDirName/code/
+
 cd $projectBaseDir/$projectDirName/code/
+echo "current runing dir: ${PWD}"
 mkdir $rawDataDir
 mkdir $postDataDir
-source ../bin/activate
-pip install ${pipArgv} -r ./$envSetupDir/required.txt
 
-# All done
+source ../bin/activate
+pip install -r ./$envSetupDir/required.txt
+
+# For Raspberry_Pi
+# sudo apt-get -qq install libxml2-dev libxslt-dev python-dev -y 
+# pip install $pipArgv -r ./$envSetupDir/required.txt
+
 sudo apt-get clean && apt-get autoremove
+
+cd $projectBaseDir
+echo "current runing dir: ${PWD}"
+
+bash ./stockproject/code/envSetup/stopsupervisor.sh
+bash ./stockproject/code/envSetup/stopsupervisor.sh
+sudo supervisorctl start mysite:*
+
 echo "If no warning, then congrats all done!"
