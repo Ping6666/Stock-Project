dbf44f8f4c9bd379430704ea7d5cddd45a89e1e2
ping <ping6666@users.noreply.github.com>
2022-08-29 11:51:52 +0800

    for docker

--- envSetup/stopsupervisor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo supervisorctl shutdown
-sudo supervisord -c /etc/supervisor/supervisord.conf
-sudo supervisorctl reread
-sudo supervisorctl update
-sudo supervisorctl stop all
-sudo systemctl stop nginx
